eaeaf3271ceaead1dc74bc6a4cb388530c6eabac
riyathapar <riyathapar06@gmail.com>
2023-01-12 20:31:03 -0500

    swipe attempt, images will not load for some reason

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,115 +1,411 @@
 import 'package:flutter/material.dart';
+class BackgroudCurveWidget extends StatelessWidget {
+  const BackgroudCurveWidget({Key? key}) : super(key: key);
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Container(
+      width: double.infinity,
+      height: 350,
+      decoration: const ShapeDecoration(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(64),
+            bottomRight: Radius.circular(64),
+          ),
+        ),
+        gradient: LinearGradient(
+          colors: <Color>[
+            Color(0xFFFD0E42),
+            Color(0xFFC30F31),
+          ],
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      child: const Padding(
+        padding: EdgeInsets.only(top: 46.0, left: 20.0),
+        child: Text(
+          'Discover',
+          style: TextStyle(
+            fontFamily: 'Nunito',
+            fontWeight: FontWeight.w800,
+            color: Colors.white,
+            fontSize: 36,
+          ),
+        ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+class Profile {
+  const Profile({
+    required this.name,
+    required this.distance,
+    required this.imageAsset,
+  });
+  final String name;
+  final String distance;
+  final String imageAsset;
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class ProfileCard extends StatelessWidget {
+  const ProfileCard({Key? key, required this.profile}) : super(key: key);
+  final Profile profile;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      height: 580,
+      width: 340,
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.asset(
+                profile.imageAsset,
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            child: Container(
+              height: 80,
+              width: 340,
+              decoration: ShapeDecoration(
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                shadows: <BoxShadow>[
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.05),
+                    blurRadius: 8,
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      profile.name,
+                      style: const TextStyle(
+                        fontFamily: 'Nunito',
+                        fontWeight: FontWeight.w800,
+                        fontSize: 21,
+                      ),
+                    ),
+                    Text(
+                      profile.distance,
+                      style: const TextStyle(
+                        fontFamily: 'Nunito',
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    );
+  }
+}
+
+enum Swipe { left, right, none }
+
+class DragWidget extends StatefulWidget {
+  const DragWidget(
+      {Key? key,
+      required this.profile,
+      required this.index,
+      required this.swipeNotifier})
+      : super(key: key);
+  final Profile profile;
+  final int index;
+  final ValueNotifier<Swipe> swipeNotifier;
+
+  @override
+  State<DragWidget> createState() => _DragWidgetState();
+}
+
+class _DragWidgetState extends State<DragWidget> {
+  ValueNotifier<Swipe> swipeNotifier = ValueNotifier(Swipe.none);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Draggable<int>(
+        // Data is the value this Draggable stores.
+        data: widget.index,
+        feedback: Material(
+          color: Colors.transparent,
+          child: ValueListenableBuilder(
+            valueListenable: swipeNotifier,
+            builder: (context, swipe, _) {
+              return RotationTransition(
+                turns: swipe != Swipe.none
+                    ? swipe == Swipe.left
+                        ? const AlwaysStoppedAnimation(-15 / 360)
+                        : const AlwaysStoppedAnimation(15 / 360)
+                    : const AlwaysStoppedAnimation(0),
+                child: Stack(
+                  children: [
+                    ProfileCard(profile: widget.profile),
+                    swipe != Swipe.none
+                        ? swipe == Swipe.right
+                            ? Positioned(
+                                top: 40,
+                                left: 20,
+                                child: Transform.rotate(
+                                  angle: 12,
+                                  child: TagWidget(
+                                    text: 'LIKE',
+                                    color: Colors.green[400]!,
+                                  ),
+                                ),
+                              )
+                            : Positioned(
+                                top: 50,
+                                right: 24,
+                                child: Transform.rotate(
+                                  angle: -12,
+                                  child: TagWidget(
+                                    text: 'DISLIKE',
+                                    color: Colors.red[400]!,
+                                  ),
+                                ),
+                              )
+                        : const SizedBox.shrink(),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+        onDragUpdate: (DragUpdateDetails dragUpdateDetails) {
+                    // When Draggable widget is dragged right
+          if (dragUpdateDetails.delta.dx > 0 &&
+              dragUpdateDetails.globalPosition.dx >
+                  MediaQuery.of(context).size.width / 2) {
+            swipeNotifier.value = Swipe.right;
+          }
+          // When Draggable widget is dragged left
+          if (dragUpdateDetails.delta.dx < 0 &&
+              dragUpdateDetails.globalPosition.dx <
+                  MediaQuery.of(context).size.width / 2) {
+            swipeNotifier.value = Swipe.left;
+          }
+        },
+        onDragEnd: (drag) {
+          swipeNotifier.value = Swipe.none;
+        },
+
+        childWhenDragging: Container(
+          color: Colors.transparent,
+        ),
+
+        child: ProfileCard(profile: widget.profile),
+      ),
+    );
+  }
+}
+
+class TagWidget extends StatelessWidget {
+  const TagWidget({
+    Key? key,
+    required this.text,
+    required this.color,
+  }) : super(key: key);
+  final String text;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      decoration: ShapeDecoration(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+          side: BorderSide(
+            color: color,
+            width: 4,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      child: Text(
+        text,
+        style: TextStyle(
+          color: color,
+          fontSize: 36,
+        ),
+      ),
+    );
+  }
+}
+
+class CardsStackWidget extends StatefulWidget {
+  const CardsStackWidget({Key? key}) : super(key: key);
+
+  @override
+  State<CardsStackWidget> createState() => _CardsStackWidgetState();
+}
+
+class _CardsStackWidgetState extends State<CardsStackWidget> {
+  List<Profile> dragabbleItems = [
+    const Profile(
+        name: 'Irene',
+        distance: '10 miles away',
+        imageAsset: 'assets/images/avatar_1.jpg'),
+    const Profile(
+        name: 'Alice',
+        distance: '10 miles away',
+        imageAsset: 'assets/images/avatar_2.jpg'),
+    const Profile(
+        name: 'Bob',
+        distance: '10 miles away',
+        imageAsset: 'assets/images/avatar_3.jpg'),
+    const Profile(
+        name: 'Carol',
+        distance: '10 miles away',
+        imageAsset: 'assets/images/avatar_4.jpg'),
+    const Profile(
+        name: 'Dave',
+        distance: '10 miles away',
+        imageAsset: 'assets/images/avatar_5.jpg'),
+  ];
+
+  ValueNotifier<Swipe> swipeNotifier = ValueNotifier(Swipe.none);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: ValueListenableBuilder(
+            valueListenable: swipeNotifier,
+            builder: (context, swipe, _) => Stack(
+              clipBehavior: Clip.none,
+              alignment: Alignment.center,
+              children: List.generate(dragabbleItems.length, (index) {
+                return DragWidget(
+                  profile: dragabbleItems[index],
+                  index: index,
+                  swipeNotifier: swipeNotifier,
+                );
+              }),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 0,
+          child: DragTarget<int>(
+            builder: (
+              BuildContext context,
+              List<dynamic> accepted,
+              List<dynamic> rejected,
+            ) {
+              return IgnorePointer(
+                child: Container(
+                  height: 700.0,
+                  width: 80.0,
+                  color: Colors.transparent,
+                ),
+              );
+            },
+            onAccept: (int index) {
+              setState(() {
+                dragabbleItems.removeAt(index);
+              });
+            },
+          ),
+        ),
+        Positioned(
+          right: 0,
+          child: DragTarget<int>(
+            builder: (
+              BuildContext context,
+              List<dynamic> accepted,
+              List<dynamic> rejected,
+            ) {
+              return IgnorePointer(
+                child: Container(
+                  height: 700.0,
+                  width: 80.0,
+                  color: Colors.transparent,
+                ),
+              );
+            },
+            onAccept: (int index) {
+              setState(() {
+                dragabbleItems.removeAt(index);
+              });
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstRoute(),
+  ));
+}
+
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Open route'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SwipePage()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
+class SwipePage extends StatelessWidget {
+  const SwipePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Stack(
+                children: const [
+                  BackgroudCurveWidget(),
+                  CardsStackWidget(),
+                ],
+          )
     );
   }
 }
